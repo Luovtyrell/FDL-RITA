40b853c4c6b59698378e05113ae1ec057843c9a3
Lucía Mª Ordoñez Vilanova <153511070+Luovtyrell@users.noreply.github.com>
2024-03-20 11:23:22 +0100

    Delete ej 4 directory

--- ej 4/arrayAvions.js
+++
@@ -1,6 +0,0 @@
-const avions = []
-avions.push(new AvioComercial("AC1", "Airbus", 4533, 20, 170, 30))
-avions.push(new Avioneta("AV1", "Remos", 700, 9, 8, 1))
-avions.push(new JetPrivat("JP1", "Eclipse", 12432, 25, 3, 2))
-
-console.table(avions)
